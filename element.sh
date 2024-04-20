0c15dbf24c4ad84c39a1dfa807658ec6a79bda8e
Sam Schlenner <SamSchlenner@gmail.com>
2024-04-20 20:19:55 +0000

    feat: look for atomic number

--- element.sh
+++ element.sh
@@ -9,12 +9,24 @@ then
 else
   # look for atomic number
   ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where atomic_number=$1")
-  echo $ATOMIC_NUMBER
-  # if no atomic number
-  # look for symbol
-  # if no symbol
-  # look for name
-  # if no name
+  if [[ -n $ATOMIC_NUMBER ]]
+  then
+    SYMBOL=$($PSQL "select symbol from elements where atomic_number=$ATOMIC_NUMBER")
+    NAME=$($PSQL "select name from elements where atomic_number=$ATOMIC_NUMBER")
+    TYPE=$($PSQL "select type from properties join types using (type_id) where atomic_number=$ATOMIC_NUMBER")
+    ATOMIC_MASS=$($PSQL "select atomic_mass from properties where atomic_number=$ATOMIC_NUMBER")
+    MELTING_POINT=$($PSQL "select melting_point_celsius from properties where atomic_number=$ATOMIC_NUMBER")
+    BOILING_POINT=$($PSQL "select boiling_point_celsius from properties where atomic_number=$ATOMIC_NUMBER")
+    echo "$ATOMIC_NUMBER" "$SYMBOL" "$NAME" "$TYPE" "$ATOMIC_MASS" "$MELTING_POINT" "$BOILING_POINT"
+  else
+    echo "No atomic number found"
+    # if no atomic number
+    # look for symbol
+    # if no symbol
+    # look for name
+    # if no name
+  fi
+
 fi
 
 
