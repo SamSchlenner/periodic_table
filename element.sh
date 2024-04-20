ca486750c4d40db5e767d9e1bcc10aa25240abac
Sam Schlenner <SamSchlenner@gmail.com>
2024-04-20 20:07:19 +0000

    feat: define PSQL, add if no argument

--- element.sh
+++ element.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# if no argument
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  # look for atomic number
+  ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where atomic_number=$1")
+  echo $ATOMIC_NUMBER
+  # if no atomic number
+  # look for symbol
+  # if no symbol
+  # look for name
+  # if no name
+fi
+
+
+
 
